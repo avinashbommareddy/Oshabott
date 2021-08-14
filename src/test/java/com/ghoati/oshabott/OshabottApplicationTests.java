c31a9391f01ec4022e88d4372f501489459c5b4c
Avinash Bommareddy <avinash2004@outlook.com>
2021-08-14 18:57:04 +0100

    initial commit

---
+++ src/test/java/com/ghoati/oshabott/OshabottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghoati.oshabott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshabottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
